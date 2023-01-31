4db9b9a1421f1c5d24a059eee18f50e9abca7ec8
Cletus Razakou <masskoder@gmail.com>
2023-01-31 00:53:17 -0500

    config: added github workflow for docs deployment

--- packages/storybook/stories/Button.stories.tsx
+++ packages/storybook/stories/Button.stories.tsx
@@ -1,36 +1,61 @@
-import { Button } from '@rotational/beacon-core';
+import { Button, BtnProps } from '@rotational/beacon-core';
+
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 export default {
   title: 'beacon/Button',
   component: Button,
+  argTypes: {
+    variant: {
+      control: {
+        type: 'select',
+        options: ['primary', 'secondary', 'tertiary', 'ghost'],
+      },
+    },
+    size: {
+      control: {
+        type: 'select',
+        options: ['small', 'medium', 'large'],
+      },
+    },
+    disabled: {
+      control: {
+        type: 'boolean',
+      },
+    },
+    isLoading: {
+      control: {
+        type: 'boolean',
+      },
+    },
+  },
   parameters: {
     actions: {
       handles: ['click .btn'],
     },
   },
-} as ComponentMeta<typeof Button>;
+} as ComponentMeta<typeof BtnProps>;
 
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+const Template: ComponentStory<BtnProps> = (args) => <Button {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
   children: 'Button',
-  leftIcon: <>😂</>,
-  rightIcon: <>😇</>,
-  isDisabled: false,
+  disabled: false,
+  isLoading: false,
+  className: '',
 };
 
 export const Primary = Template.bind({});
 Primary.args = {
   ...Default.args,
-  color: 'primary',
+  variant: 'primary',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
   ...Default.args,
-  color: 'secondary',
+  variant: 'secondary',
 };
 
 export const Tertiary = Template.bind({});
@@ -43,4 +68,36 @@ export const Ghost = Template.bind({});
 Ghost.args = {
   ...Default.args,
   variant: 'ghost',
+};
+
+export const onClickButton = Template.bind({});
+onClickButton.args = {
+  ...Default.args,
+  onClick: () => alert('onClick works!'),
+};
+
+export const roundedButton = Template.bind({});
+
+roundedButton.args = {
+  ...Default.args,
+  className: 'rounded-full',
+};
+
+export const ButtonWithLeftIcon = Template.bind({});
+ButtonWithLeftIcon.args = {
+  ...Default.args,
+  leftIcon: '👍',
+};
+
+export const ButtonWithRightIcon = Template.bind({});
+ButtonWithRightIcon.args = {
+  ...Default.args,
+  rightIcon: '👍',
+};
+
+export const ButtonWithLeftAndRightIcon = Template.bind({});
+ButtonWithLeftAndRightIcon.args = {
+  ...Default.args,
+  leftIcon: '👍',
+  rightIcon: '👍',
 };
---
+++ packages/storybook/stories/Table.stories.tsx
@@ -0,0 +1,102 @@
+// import { Table } from '@rotational/beacon-core';
+
+// import { Meta, Story } from '@storybook/react';
+
+// export default {
+//   title: 'beacon/Table',
+//   component: Table,
+// } as Meta<typeof Table>;
+
+// const Template: Story<typeof Table> = (args) => <Table {...args} />;
+
+// export const Default = Template.bind({});
+// Default.args = {
+//   columns: [
+//     {
+//       Header: 'Name',
+//       columns: [
+//         {
+//           Header: 'First Name',
+//           accessor: 'firstName',
+//         },
+//         {
+//           Header: 'Last Name',
+//           accessor: 'lastName',
+//         },
+//       ],
+//     },
+//     {
+//       Header: 'Info',
+//       columns: [
+//         {
+//           Header: 'Age',
+//           accessor: 'age',
+//         },
+//         {
+//           Header: 'Visits',
+//           accessor: 'visits',
+//         },
+//         {
+//           Header: 'Status',
+//           accessor: 'status',
+//         },
+//         {
+//           Header: 'Profile Progress',
+//           accessor: 'progress',
+//         },
+//       ],
+//     },
+//   ],
+//   data: [
+//     {
+//       firstName: 'Jeff',
+//       lastName: 'Bezos',
+//       age: 23,
+//       visits: 34,
+//       status: 'relationship',
+//       progress: 98,
+//     },
+//     {
+//       firstName: 'Bill',
+//       lastName: 'Gates',
+//       age: 45,
+//       visits: 32,
+//       status: 'complicated',
+//       progress: 60,
+//     },
+//     {
+//       firstName: 'Mark',
+//       lastName: 'Zuckerberg',
+//       age: 78,
+//       visits: 43,
+//       status: 'single',
+//       progress: 80,
+//     },
+//     {
+//       firstName: 'Paul',
+//       lastName: 'Allen',
+//       age: 56,
+//       visits: 23,
+//       status: 'relationship',
+//       progress: 100,
+//     },
+//     {
+//       firstName: 'Steve',
+//       lastName: 'Ballmer',
+
+//       age: 23,
+//       visits: 34,
+//       status: 'relationship',
+//       progress: 98,
+//     },
+//     {
+//       firstName: 'Larry',
+//       lastName: 'Ellison',
+//       age: 45,
+//       visits: 32,
+//       status: 'complicated',
+//       progress: 60,
+//     },
+//   ],
+// };
+export {};
