73c9aa29ba820efa0a08f8cc6f933b8eaa35e4f8
Elysee Bleu <54010836+elysee15@users.noreply.github.com>
2023-04-27 17:03:47 +0000

    remove AriaButton component (#32)

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,6 @@
 export * from './utils';
 export * from './hooks';
 export * from './components/Button';
-export * from './components/AriaButton';
 export * from './components/Card';
 export * from './components/Grid';
 export * from './components/Loader';
