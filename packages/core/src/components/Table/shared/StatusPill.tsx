67bf40bb2f863a6604657a964b699f753e3c2fd4
Cletus Razakou <masskoder@gmail.com>
2023-05-18 12:32:13 -0400

    clean up status style

--- packages/core/src/components/Table/shared/StatusPill.tsx
+++ packages/core/src/components/Table/shared/StatusPill.tsx
@@ -1,5 +1,5 @@
 import mergeClassnames from '../../../utils/mergeClassnames';
-import { STATUS, capitalize } from './util';
+import { STATUS } from './util';
 import { StatusColorIcon } from '../../Icon/Icons';
 interface StatusPillProps {
   value: any;
@@ -9,19 +9,25 @@ export function StatusPill({ value, className }: StatusPillProps) {
   const status = value ? value.toLowerCase() : 'unknown';
 
   const statusColorMap = {
-    [STATUS.ACTIVE || STATUS.CONFIRMED || STATUS.COMPLETE]: 'text-green-800',
-    [STATUS.INACTIVE || STATUS.INCOMPLETE]: 'text-warning-300',
-    [STATUS.REVOKED]: 'text-warning-600',
+    [STATUS.ACTIVE]: 'text-green-800',
+    [STATUS.CONFIRMED]: 'text-green-800',
+    [STATUS.COMPLETE]: 'text-green-800',
+    [STATUS.PENDING]: 'text-warning-500',
+    [STATUS.INCOMPLETE]: 'text-warning-500',
+    [STATUS.INACTIVE]: 'text-warning-500',
+    [STATUS.REVOKED]: 'text-primary-500',
     [STATUS.ERROR]: 'text-danger-600',
     [STATUS.UNUSED]: 'text-gray-600',
   } as any;
 
   const statusIconMap = {
-    [STATUS.ACTIVE || STATUS.CONFIRMED || STATUS.COMPLETE]: (
-      <StatusColorIcon fill="#34753E" />
-    ),
-    [STATUS.INACTIVE || STATUS.INCOMPLETE]: <StatusColorIcon fill="#FFCC75" />,
-    [STATUS.REVOKED]: <StatusColorIcon fill="#CC7C00" />,
+    [STATUS.ACTIVE]: <StatusColorIcon fill="#34753E" />,
+    [STATUS.COMPLETE]: <StatusColorIcon fill="#34753E" />,
+    [STATUS.CONFIRMED]: <StatusColorIcon fill="#34753E" />,
+    [STATUS.INACTIVE]: <StatusColorIcon fill="#FF9500" />,
+    [STATUS.PENDING]: <StatusColorIcon fill="#FF9500" />,
+    [STATUS.INCOMPLETE]: <StatusColorIcon fill="#FF9500" />,
+    [STATUS.REVOKED]: <StatusColorIcon fill="#F26800" />,
     [STATUS.ERROR]: <StatusColorIcon fill="#EB2A00" />,
     [STATUS.UNUSED]: <StatusColorIcon fill="#6C757D" />,
   } as any;
@@ -32,7 +38,7 @@ export function StatusPill({ value, className }: StatusPillProps) {
       <span
         className={mergeClassnames('ml-1', statusColorMap[status] as string)}
       >
-        {capitalize(status)}
+        {value}
       </span>
     </div>
   );
